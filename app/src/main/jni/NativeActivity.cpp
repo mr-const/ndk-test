3754fd9f7e05846a9eab981c028af8697edef74b
Konst Kolesnichenko <const@dynamic-systems.com.ua>
2015-08-03 12:52:44 +0300

    * application which starts and runs

--- app/src/main/jni/NativeActivity.cpp
+++ app/src/main/jni/NativeActivity.cpp
@@ -1,1 +1,9 @@
 #include <jni.h>
+#include "android_native_app_glue.h"
+/**
+ * This is the main entry point of a native application that is using
+ * android_native_app_glue.  It runs in its own thread, with its own
+ * event loop for receiving input events and doing other things.
+ */
+void android_main( android_app* state ) {
+}
